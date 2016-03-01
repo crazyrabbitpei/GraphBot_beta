8ad5bfdf2a73d2ef5a74b87d436c660b4f5167e6
crazyrabbitpei <willow111333@gmail.com>
2016-03-01 16:35:38 +0800

    ok version2

--- run.js
+++ run.js
@@ -96,15 +96,16 @@ function setpromise(){
                 fs.appendFile('./log/'+date+'.oklist',"start:"+date_start+"\nend:"+date_end+"\n"+crawled_id+"\n--\n",function(){
                 });
             }
-            if(current_url<10){
+            if(success_url<10){
                 setpromise();          
             }
             else{
-                console.log("Finish:"+current_url);
+                console.log("Finish:"+success_url);
             }
         }
         else if(stat=='none'){
-            console.log('All crawled');
+            console.log(crawled_id+' all be crawled ');
+            setpromise();          
         }
         else if(stat.indexOf('error')!=-1){
             console.log("error occur:"+stat);
@@ -217,8 +218,6 @@ function get_accessToken(fin){
 function setBot(botkey,groupid,token,fin){
     console.log("--\ngo groupid:"+groupid);
     try{
-        var start_d  = new Date();
-        var date_start = dateFormat(start_d, "yyyymmdd_HHMM");
         fbBot.crawlerFB(token,groupid,botkey,function(result){
             current_url++;
             console.log("current num:"+current_url);
@@ -227,17 +226,6 @@ function setBot(botkey,groupid,token,fin){
                 fin(result);
             }
             else{
-                let now  = new Date();
-                let date = dateFormat(now, "yyyymmdd");
-                let parts = result.split("endTONext@Gais:");
-                let crawled_id = parts[1];
-                let end_d  = new Date();
-                let date_end = dateFormat(end_d, "yyyymmdd_HHMM");
-                if(crawled_id!="crawled"){
-                    fs.appendFile('./log/'+date+'.oklist.test',"start:"+date_start+"\nend:"+date_end+"\n"+crawled_id+"\n--\n",function(){
-                    });
-                }
-
                 fin(result);
             }
         });
